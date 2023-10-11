61f625dacbf4555dedfcc8b0543b6b8a4335719a
BorjaRuizReverter <borjaruizreverter@gmail.com>
2023-10-11 17:33:53 -0300

    style: cleaning code

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ import mysql from 'mysql2';
 
 dotenv.config();
 
-//var nodeEnv = process.env.NODE_ENV;
-
+//app is an instance of express
 var app = express();
+
+//Specifying middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Set up connection to database.
 var connection = mysql.createConnection(process.env.DATABASE_URL);
 
@@ -20,32 +25,19 @@ connection.connect((err) => {
   console.log('Connected as ID ' + connection.threadId);
 });
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-
-// Listen to POST requests to /contacts.
-// app.post('/contacts', function(req, res) {
-//   // Sent data.
-//   // var Name = req.body.Name;
-//   // var Email = req.body.Email;
-//   // Do a MySQL query.
-//   const { Name, Email } = req.body;
-//   var query = connection.query('INSERT INTO contacts (Name, Email) VALUES (?,?)', [Name,Email], function(err, result) {
-//   });
-//   res.end('Success');
-// });
-
+//Route Structure:
+//app.METHOD(PATH, HANDLER), 
+//where PATH is a path on the server and HANDLER is the function executed when the route is matched.
 app.post('/contacts', (req, res) => {
   const { Name, Email } = req.body;
+  // var Name = req.body.Name;
+  // var Email = req.body.Email;
   connection.query('INSERT INTO contacts (Name, Email) VALUES (?, ?)', [Name, Email], (err, result) => {
       if (err) {
           console.error('Error when inserting contact: ' + err.message);
           res.status(500).send('Error when inserting contact: ' + err.message);
           return;
       }
-
       res.status(201).json({ message: 'Successfully entered contact!' });
   });
 });
@@ -59,19 +51,9 @@ app.get('/contacts', (req, res) => {
           res.status(500).send('Error retrieving contacts');
           return;
       }
-
       res.json(results);
   });
 });
-
-// simple query
-// connection.query(
-//   'SELECT id FROM contacts',
-//   (err, results, fields) => {
-//     console.log(results); // results contains rows returned by server
-//     console.log(fields); // fields contains extra meta data about results, if available
-//   }
-// );
 
 app.listen(3000, function() {
   console.log('Listening on port 3000!');
